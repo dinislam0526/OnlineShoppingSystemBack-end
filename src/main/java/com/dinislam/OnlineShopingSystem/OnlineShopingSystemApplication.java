1ed5d0b79e097a3243ad0aec2839c04ab7ff4e6b
dinislam2698 <105793400+dinislam2698@users.noreply.github.com>
2023-01-22 23:11:26 +0600

    Online Shoping

---
+++ src/main/java/com/dinislam/OnlineShopingSystem/OnlineShopingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dinislam.OnlineShopingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopingSystemApplication.class, args);
+	}
+
+}
