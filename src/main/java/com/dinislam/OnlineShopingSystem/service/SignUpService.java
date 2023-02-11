eab269ab74f030f41978bddf1879c2b3d68767b5
dinislam2698 <105793400+dinislam2698@users.noreply.github.com>
2023-02-11 12:53:34 +0600

    commit_11/02/23

--- src/main/java/com/dinislam/OnlineShopingSystem/service/SignUpService.java
+++ src/main/java/com/dinislam/OnlineShopingSystem/service/SignUpService.java
@@ -1,11 +1,9 @@
 package com.dinislam.OnlineShopingSystem.service;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.dinislam.OnlineShopingSystem.model.Product;
 import com.dinislam.OnlineShopingSystem.model.SignUp;
 import com.dinislam.OnlineShopingSystem.repository.SignUpRepository;
 
